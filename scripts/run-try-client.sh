614b033a89f3b14bc1a41856ae5b259649a56681
Peter Goldsborough <peter@goldsborough.me>
2016-08-28 17:45:29 +0100

    Made run scripts more cross-platform

--- scripts/run-try-client.sh
+++ scripts/run-try-client.sh
@@ -1,4 +1,8 @@
 #!/bin/zsh
 
-DYLD_INSERT_LIBRARIES=$PWD/../source/tssx/libtssx-client.dylib \
-DYLD_FORCE_FLAT_NAMESPACE=1 ./try-client
+if [ ${OSTYPE//[0-9.]/} = darwin ]; then
+  DYLD_INSERT_LIBRARIES=$PWD/../source/tssx/libtssx-client.dylib \
+  DYLD_FORCE_FLAT_NAMESPACE=1 ./try-client
+else
+  LD_PRELOAD=$PWD/../source/tssx/libtssx-client.so ./try-client
+fi
--- scripts/run-try-server.sh
+++ scripts/run-try-server.sh
@@ -4,5 +4,9 @@ cd ../source/tssx
 make
 cd -
 
-DYLD_INSERT_LIBRARIES=$PWD/../source/tssx/libtssx-server.dylib \
-DYLD_FORCE_FLAT_NAMESPACE=1 ./try-server $1
+if [ ${OSTYPE//[0-9.]/} = darwin ]; then
+  DYLD_INSERT_LIBRARIES=$PWD/../source/tssx/libtssx-server.dylib \
+  DYLD_FORCE_FLAT_NAMESPACE=1 ./try-server $1
+else
+  LD_PRELOAD=$PWD/../source/tssx/libtssx-server.so ./try-server
+fi
